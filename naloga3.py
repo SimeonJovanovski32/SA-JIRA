5ca44621b8c2fcb39733fa1a146b094f4dbfadf4
SimeonJovanovski32 <simeon.jovanovski@student.um.si>
2025-05-02 18:23:18 +0200

    Update naloga3.py Izracunaj centre

--- naloga3.py
+++ naloga3.py
@@ -36,9 +36,49 @@ def meanshift(slika, velikost_okna, dimenzija):
     '''Izvede segmentacijo slike z uporabo metode mean-shift.'''
     pass
 
+
 def izracunaj_centre(slika, izbira, dimenzija_centra, T):
     '''Izračuna centre za metodo kmeans.'''
-    pass
+    # Pridobimo dimenzije slike
+    visina, sirina, _ = slika.shape
+
+    # Pretvorimo sliko v 2D tabelo, kjer vsaka vrstica predstavlja en piksel in njegove RGB vrednosti
+    slika_reshape = slika.reshape((-1, 3))
+
+    if dimenzija_centra == 5:
+        # Dodamo koordinate (X, Y) vsakega piksla
+        coordinates = np.column_stack(np.indices((visina, sirina)))
+        slika_reshape = np.hstack([slika_reshape, coordinates.reshape(-1, 2)])
+
+    # Izbira centrov
+    if izbira == 'ročno':
+        # Tukaj je potrebna implementacija za ročno izbiro centrov (s klikom na sliko)
+        # Predpostavljamo, da imate neko orodje za to, ki vrača seznam izbranih centrov.
+        print("Ročna izbira centrov je potrebna")
+        return []
+
+    elif izbira == 'naključna':
+        # Naključna izbira centrov
+        k = 3  # Ali prilagodite k, kako ga določiti
+        centroids = []
+
+        # Naključno izberemo k začetnih centrov, preverimo razdalje med njimi
+        while len(centroids) < k:
+            # Naključen indeks piksla
+            index = np.random.choice(slika_reshape.shape[0])
+            new_centroid = slika_reshape[index]
+
+            # Preverimo, če so centri dovolj oddaljeni
+            if len(centroids) == 0:
+                centroids.append(new_centroid)
+            else:
+                distances = np.linalg.norm(np.array(centroids)[:, :dimenzija_centra] - new_centroid[:dimenzija_centra], axis=1)
+                if np.all(distances > T):
+                    centroids.append(new_centroid)
+
+        return np.array(centroids)
+    
+
 
 if __name__ == "__main__":
     pass
